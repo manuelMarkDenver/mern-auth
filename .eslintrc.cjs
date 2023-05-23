7b47f3ec205b60817b2aa9f8bdfe124d12348b17
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-05-23 20:43:29 +0800

    working user register

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "comma-dangle": "off",
+  },
+};
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,26 @@
-import express from 'express';
-const port = 5000;
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API running'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => console.log(`Server running in port ${port}`));
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
